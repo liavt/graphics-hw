45df81e3f0f3a1c89b9d5746d3e64987dcc51b03
Liav Turkia <liav.turkia@gmail.com>
2022-03-02 11:36:02 +0200

    Add files via upload

---
+++ ex1/seam_carving.py
@@ -0,0 +1,62 @@
+def get_greyscale_image(image, colour_wts):
+    """
+    Gets an image and weights of each colour and returns the image in greyscale
+    :param image: The original image
+    :param colour_wts: the weights of each colour in rgb (ints > 0)
+    :returns: the image in greyscale
+    """
+    
+    ###Your code here###
+    ###**************###
+    return greyscale_image
+    
+def reshape_bilinear(image, new_shape):
+    """
+    Resizes an image to new shape using bilinear interpolation method
+    :param image: The original image
+    :param new_shape: a (height, width) tuple which is the new shape
+    :returns: the image resized to new_shape
+    """
+    in_height, in_width, _ = image.shape
+    out_height, out_width = new_shape
+    new_image = np.zeros(new_shape)
+    ###Your code here###
+    ###**************####
+    return new_image
+    
+def gradient_magnitude(image, colour_wts):
+    """
+    Calculates the gradient image of a given image
+    :param image: The original image
+    :param colour_wts: the weights of each colour in rgb (> 0) 
+    :returns: The gradient image
+    """
+    greyscale = get_greyscale_image(image, colour_wts)
+    ###Your code here###
+    ###**************###
+    return gradient
+    
+def visualise_seams(image, new_shape, carving_scheme, colour):
+    """
+    Visualises the seams that would be removed when reshaping an image to new image (see example in notebook)
+    :param image: The original image
+    :param new_shape: a (height, width) tuple which is the new shape
+    :param carving_scheme: the carving scheme to be used.
+    :param colour: the colour of the seams (an array of size 3)
+    :returns: an image where the removed seams have been coloured.
+    """
+    ###Your code here###
+    ###**************###
+    return seam_image
+    
+def reshape_seam_crarving(image, new_shape, carving_scheme):
+"""
+    Resizes an image to new shape using seam carving
+    :param image: The original image
+    :param new_shape: a (height, width) tuple which is the new shape
+    :param carving_scheme: the carving scheme to be used.
+    :returns: the image resized to new_shape
+    """
+    ###Your code here###
+    ###**************###
+    return new_image
