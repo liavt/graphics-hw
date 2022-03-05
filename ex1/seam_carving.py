397dfd2206c1385746ff04f28f9f016378040aeb
liavt <liav.turkia@gmail.com>
2022-03-05 22:52:43 +0200

    adkasfhkajhdgak

--- ex1/seam_carving.py
+++ ex1/seam_carving.py
@@ -1,3 +1,7 @@
+import math
+import numpy as np
+import itertools
+
 def get_greyscale_image(image, colour_wts):
     """
     Gets an image and weights of each colour and returns the image in greyscale
@@ -5,24 +9,40 @@ def get_greyscale_image(image, colour_wts):
     :param colour_wts: the weights of each colour in rgb (ints > 0)
     :returns: the image in greyscale
     """
+    return image @ colour_wts
     
-    ###Your code here###
-    ###**************###
-    return greyscale_image
-    
-def reshape_bilinear(image, new_shape):
+def reshape_bilinear(img, new_shape):
     """
     Resizes an image to new shape using bilinear interpolation method
     :param image: The original image
     :param new_shape: a (height, width) tuple which is the new shape
     :returns: the image resized to new_shape
     """
-    in_height, in_width, _ = image.shape
-    out_height, out_width = new_shape
-    new_image = np.zeros(new_shape)
-    ###Your code here###
-    ###**************####
-    return new_image
+    image = img.reshape((img.shape[0] * img.shape[1], 3))
+
+    y, x = np.divmod(np.arange(new_shape[0] * new_shape[1]), new_shape[1])
+
+    x_indices = (float(img.shape[1]) / (new_shape[1]) if new_shape[1] >= 1 else 0) * x
+    y_indices = (float(img.shape[0]) / (new_shape[0]) if new_shape[0] >= 1 else 0) * y
+
+    x_left = np.floor(x_indices).astype('uint32')
+    y_bottom = np.floor(y_indices).astype('uint32')
+
+    x_right = np.ceil(x_indices).astype('uint32')
+    y_top = np.ceil(y_indices).astype('uint32')
+
+    x_coefficients = (x_indices - x_left).repeat(3).reshape((new_shape[1] * new_shape[0], 3))
+    y_coefficients = (y_indices - y_top).repeat(3).reshape((new_shape[1] * new_shape[0], 3))
+
+    y_bottom *= img.shape[1]
+    y_top *= img.shape[1]
+
+    a = image[y_bottom + x_left] * (1 - x_coefficients) * (1 - y_coefficients)
+    b = image[y_bottom + x_right] * x_coefficients * (1 - y_coefficients)
+    c = image[y_top + x_left] * (1 - x_coefficients) * y_coefficients
+    d = image[y_top + x_right] * x_coefficients * y_coefficients
+
+    return (a + b + c + d).astype('uint8').reshape((new_shape[0], new_shape[1], 3))
     
 def gradient_magnitude(image, colour_wts):
     """
@@ -32,9 +52,17 @@ def gradient_magnitude(image, colour_wts):
     :returns: The gradient image
     """
     greyscale = get_greyscale_image(image, colour_wts)
-    ###Your code here###
-    ###**************###
-    return gradient
+
+    # shift the image by one pixel
+    horz_shifted = np.empty_like(greyscale)
+    horz_shifted[:1] = 0
+    horz_shifted[1:] = greyscale[:-1]
+
+    vert_shifted = np.empty_like(greyscale)
+    vert_shifted[:,:1] = 0
+    vert_shifted[:,1:] = greyscale[:,:-1]
+
+    return np.sqrt(np.power(greyscale - horz_shifted, 2) + np.power(greyscale - vert_shifted, 2))
     
 def visualise_seams(image, new_shape, carving_scheme, colour):
     """
@@ -47,10 +75,10 @@ def visualise_seams(image, new_shape, carving_scheme, colour):
     """
     ###Your code here###
     ###**************###
-    return seam_image
+    #return seam_image
     
 def reshape_seam_crarving(image, new_shape, carving_scheme):
-"""
+    """
     Resizes an image to new shape using seam carving
     :param image: The original image
     :param new_shape: a (height, width) tuple which is the new shape
@@ -59,4 +87,4 @@ def reshape_seam_crarving(image, new_shape, carving_scheme):
     """
     ###Your code here###
     ###**************###
-    return new_image
+    #return new_image
