650275332b64a3aba91a99d11b37d64401e8d0a4
Ilana Sivan <ilana@Ilanas-MacBook-Pro-2.local>
2022-03-12 11:02:04 -0500

    sowwy uwu

--- ex1/seam_carving.py
+++ ex1/seam_carving.py
@@ -63,7 +63,95 @@ def gradient_magnitude(image, colour_wts):
     vert_shifted[:,1:] = greyscale[:,:-1]
 
     return np.sqrt(np.power(greyscale - horz_shifted, 2) + np.power(greyscale - vert_shifted, 2))
+
+def energy(image, forward=False):
+    """
+    Helper function to calculate forward energy
+    :param image: The original image
+    """
     
+    height, width = image.shape[:2]
+    image = gradient_magnitude(image, [0.299, 0.587, 0.114])
+    
+    energy = np.zeros((height,width))
+    
+    if forward:
+        for i in range(1, height):
+            for j in range(width):
+                up = (i-1) % height
+                down = (i+1) % height
+                left = (j-1) % width
+                right = (j+1) % width
+
+                U = energy[up,j]
+                L = energy[up,left]
+                R = energy[up,right]
+
+                cU = np.abs(image[i,right] - image[i,left])
+                cL = np.abs(image[up,j] - image[i,left]) + cU
+                cR = np.abs(image[up,j] - image[i,right]) + cU
+
+                cULR = np.array([cU, cL, cR])
+                mULR = np.array([U, L, R]) + cULR
+
+                argmin = np.argmin(mULR)
+                energy[i,j] = cULR[argmin]        
+
+        return energy
+            
+    else:
+        for i in range(height):
+            for j in range(width):
+                left = image[i, (j-1) % width]
+                right = image[i, (j+1) % width]
+                up = image[(i-1) % height, j]
+                down = image[(i+1) % height, j]
+
+                dx = np.sum((right - left)**2)
+                dy = np.sum((down - up)**2)
+                energy[i,j] = np.sqrt(dx + dy)
+        return energy
+        
+def cum_map(image):
+    height, width, _ = image.shape
+    map_copy = energy(image, forward=True)
+    skip = np.zeros_like(map_copy, dtype=np.int)
+
+    for i in range(1, height):
+        for j in range(0, width):
+
+            if j == 0:
+                idx = np.argmin(map_copy[i - 1, j:j + 2])
+                skip[i, j] = idx + j
+                min_energy = map_copy[i - 1, idx + j]
+            else:
+                idx = np.argmin(map_copy[i - 1, j - 1:j + 2])
+                skip[i, j] = idx + j - 1
+                min_energy = map_copy[i - 1, idx + j - 1]
+
+            map_copy[i, j] += min_energy
+
+    return map_copy, skip
+
+def calc_seam(image, seams):
+    height, width, _ = image.shape
+    image_map, skip = cum_map(image)
+
+    mask = np.ones((height, width), dtype=np.bool)
+   
+    for i in range(seams):
+        smallest = np.argmin(image_map[-1])
+
+        for j in reversed(range(height)):
+            mask[j, smallest] = False
+            image_map[j,smallest] = 1000000
+            smallest = skip[j, smallest]
+
+    #mask = ([mask] * 3, axis=2)
+    #image = image[mask].reshape((height, width - 1, 3))
+
+    return mask
+        
 def visualise_seams(image, new_shape, carving_scheme, colour):
     """
     Visualises the seams that would be removed when reshaping an image to new image (see example in notebook)
@@ -74,6 +162,18 @@ def visualise_seams(image, new_shape, carving_scheme, colour):
     :returns: an image where the removed seams have been coloured.
     """
     ###Your code here###
+    
+    height, width, _ = image.shape
+    num_seams = width-new_shape[1]
+    mask = calc_seam(image, num_seams)
+    image_copy = image.copy()
+    
+    for i in range(height):
+        for j in range(width):
+            if mask[i,j]:
+                image_copy[i,j] = colour
+        
+    return image_copy    
     ###**************###
     #return seam_image
     
@@ -86,5 +186,15 @@ def reshape_seam_crarving(image, new_shape, carving_scheme):
     :returns: the image resized to new_shape
     """
     ###Your code here###
+    
+    height, width, _ = image.shape
+    
+    num_seams = width-new_shape[1]
+    
+    for i in range(width - new_shape[1]): 
+        image = cut_seam(image)
+
+    return image
+        
     ###**************###
     #return new_image
