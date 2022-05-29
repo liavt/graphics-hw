ce64c1c3677529988b13e00b9128e6113ba60e4a
Ilana Sivan <ilana@Ilanas-MacBook-Pro-2.local>
2022-05-29 15:30:54 -0400

    look mom

--- Exercise 5/src/hw5.js
+++ Exercise 5/src/hw5.js
@@ -79,15 +79,117 @@ windows.applyMatrix4(windowRotation);
 hull.add( windows );
 
 const planetGeometry = new THREE.SphereGeometry(15, 80, 780);
-const planetMaterial = new THREE.MeshPhongMaterial()
-planetMaterial.map = THREE.ImageUtils.loadTexture('https://eoimages.gsfc.nasa.gov/images/imagerecords/4000/4702/aster_volcanoes_artII_lrg.jpg');
+const planetMaterial = new THREE.MeshPhongMaterial({color: 0x243652})
 const planet = new THREE.Mesh(planetGeometry, planetMaterial);
 const planetTranslate = new THREE.Matrix4();
 planetTranslate.makeTranslation(30,-15,10);
 planet.applyMatrix4(planetTranslate);
 scene.add(planet);
-
+// ORBIT
+// this works added to scene also *but* it is very far so here please observe him from a close distance
 planet.add(hull);
+
+const islandGeometry = new THREE.SphereGeometry(5,5,10);
+const islandMaterial = new THREE.MeshPhongMaterial({color: 0xc9aa7f});
+const island = new THREE.Mesh(islandGeometry, islandMaterial);
+const islandTranslate = new THREE.Matrix4();
+islandTranslate.makeTranslation(0,12,-2);
+island.applyMatrix4(islandTranslate);
+planet.add(island);
+
+//PalmTree
+const palmTree = new THREE.Group();
+palmTree.position.set(0,5,-1);
+
+// Trunk
+const palmTrunkShape = new THREE.CylinderGeometry(.3,1,10,6);
+const palmTrunkMaterial = new THREE.MeshPhongMaterial({color: 0x3d2e1a});
+const palmTrunk = new THREE.Mesh(palmTrunkShape, palmTrunkMaterial);
+palmTree.add(palmTrunk)
+
+// Leaves
+const leafGroup1 = new THREE.Group();
+leafGroup1.position.set(0,5.1,-1);
+
+const leafShape = new THREE.BoxGeometry(1,.3,4);
+const leafMaterial = new THREE.MeshPhongMaterial({color:0x1f400e});
+const leaf1 = new THREE.Mesh(leafShape, leafMaterial);
+const leaf1Translate = new THREE.Matrix4();
+leaf1Translate.makeTranslation(0,-1,-1);
+leaf1.applyMatrix4(leaf1Translate);
+leaf1.rotation.x = -Math.PI/8;
+
+const leaf2 = new THREE.Mesh(leafShape, leafMaterial);
+const leaf2Translate = new THREE.Matrix4();
+leaf2Translate.makeTranslation(-1.3,-.3,1.3);
+leaf2.applyMatrix4(leaf2Translate);
+leaf2.rotation.x = -Math.PI/32;
+leaf2.rotation.y = -Math.PI/2;
+leaf2.rotation.z = -Math.PI/64;
+
+const leaf3 = new THREE.Mesh(leafShape, leafMaterial);
+const leaf3Translate = new THREE.Matrix4();
+leaf3Translate.makeTranslation(-.3,-1,3);
+leaf3.applyMatrix4(leaf3Translate);
+leaf3.rotation.x = Math.PI/8;
+leaf3.rotation.y = -Math.PI/8;
+leaf3.rotation.z = -Math.PI/32;
+
+const leaf4 = new THREE.Mesh(leafShape, leafMaterial);
+const leaf4Translate = new THREE.Matrix4();
+leaf4Translate.makeTranslation(2,0,1);
+leaf4.applyMatrix4(leaf4Translate);
+leaf4.rotation.x = Math.PI/16;
+leaf4.rotation.y = -Math.PI/2;
+leaf4.rotation.z = Math.PI/16;
+
+leafGroup1.add(leaf1);
+leafGroup1.add(leaf2);
+leafGroup1.add(leaf3);
+leafGroup1.add(leaf4);
+
+const leafGroup2 = new THREE.Group();
+leafGroup2.position.set(0,4.6,-1);
+const leaf2Material = new THREE.MeshPhongMaterial({color:0x1f400e});
+const leaf5 = new THREE.Mesh(leafShape, leafMaterial);
+const leaf5Translate = new THREE.Matrix4();
+leaf5Translate.makeTranslation(0,-1,-1);
+leaf5.applyMatrix4(leaf5Translate);
+leaf5.rotation.x = -Math.PI/8;
+
+const leaf6 = new THREE.Mesh(leafShape, leafMaterial);
+const leaf6Translate = new THREE.Matrix4();
+leaf6Translate.makeTranslation(-1.3,-.3,1.3);
+leaf6.applyMatrix4(leaf6Translate);
+leaf6.rotation.x = -Math.PI/32;
+leaf6.rotation.y = -Math.PI/2;
+leaf6.rotation.z = -Math.PI/64;
+
+const leaf7 = new THREE.Mesh(leafShape, leafMaterial);
+const leaf7Translate = new THREE.Matrix4();
+leaf7Translate.makeTranslation(-.3,-1,3);
+leaf7.applyMatrix4(leaf7Translate);
+leaf7.rotation.x = Math.PI/8;
+leaf7.rotation.y = -Math.PI/8;
+leaf7.rotation.z = -Math.PI/32;
+
+const leaf8 = new THREE.Mesh(leafShape, leafMaterial);
+const leaf8Translate = new THREE.Matrix4();
+leaf8Translate.makeTranslation(2,-1.3,2);
+leaf8.applyMatrix4(leaf8Translate);
+leaf8.rotation.x = -Math.PI/32;
+leaf8.rotation.y = -Math.PI/2;
+leaf8.rotation.z = Math.PI/16;
+
+leafGroup2.add(leaf5);
+leafGroup2.add(leaf6);
+leafGroup2.add(leaf7);
+leafGroup2.add(leaf8);
+
+palmTree.add(leafGroup1)
+//palmTree.add(leafGroup2)
+palmTree.rotation.z = Math.PI/32;
+island.add(palmTree)
 
 const starGeometry = new THREE.BufferGeometry();
 const starMaterial = new THREE.PointsMaterial({color: 0xffffff, size:0.1, sizeAttenuation: false})
@@ -102,19 +204,41 @@ starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVerti
 const stars = new THREE.Points(starGeometry, starMaterial);
 scene.add(stars);
 
-const eyeGeometry = new THREE.SphereGeometry();
+const eyeGeometry = new THREE.SphereGeometry(2, 10, 10);
 const eyeMaterial = new THREE.MeshPhongMaterial( {color: 0xffffff});
 const eye = new THREE.Mesh(eyeGeometry, eyeMaterial);
 const eyeTranslate = new THREE.Matrix4();
-eyeTranslate.makeTranslation();
+eyeTranslate.makeTranslation(30,15,20);
 eye.applyMatrix4(eyeTranslate);
 scene.add(eye)
 
-const irisGeometry = new THREE.SphereGeometry();
+const irisGeometry = new THREE.SphereGeometry(1.5,7.5, 7.5);
+const irisMaterial = new THREE.MeshPhongMaterial( {color: 0x1f5222});
+const iris = new THREE.Mesh(irisGeometry, irisMaterial);
+const irisTranslate = new THREE.Matrix4();
+irisTranslate.makeTranslation(0,-.8,-.8);
+iris.applyMatrix4(irisTranslate);
+eye.add(iris);
+
+const pupilGeometry = new THREE.SphereGeometry(1,5, 5);
+const pupilMaterial = new THREE.MeshPhongMaterial( {color: 0x000000});
+const pupil = new THREE.Mesh(pupilGeometry, pupilMaterial);
+const pupilTranslate = new THREE.Matrix4();
+pupilTranslate.makeTranslation(0,-1.4,-1.4);
+pupil.applyMatrix4(pupilTranslate);
+eye.add(pupil);
+
+const angryGeometry = new THREE.CylinderGeometry(1,4,20,6);
+const angryMaterial = new THREE.MeshPhongMaterial({color:0xd43939, transparent: true, opacity:.4});
+const angry = new THREE.Mesh(angryGeometry, angryMaterial);
+const angryTranslate = new THREE.Matrix4();
+angryTranslate.makeTranslation(0,-7,-7);
+angry.applyMatrix4(angryTranslate);
+angry.rotation.x = Math.PI/4;
+eye.add(angry);
 
 
 scene.add( new THREE.AmbientLight( 0xffffff))
-
 
 // This defines the initial distance of the camera
 const cameraTranslate = new THREE.Matrix4();
@@ -172,12 +296,12 @@ function animate() {
   if (animation1) {
     const animationRotation = new THREE.Matrix4();
     animationRotation.makeRotationZ(-0.001);
-    planet.applyMatrix4(animationRotation);
+    hull.applyMatrix4(animationRotation);
   }
   if (animation2) {
     const animationRotation = new THREE.Matrix4();
     animationRotation.makeRotationY(-0.001);
-    planet.applyMatrix4(animationRotation);
+    hull.applyMatrix4(animationRotation);
   }
   if (animation3) {
     const animationTranslation = new THREE.Matrix4();
